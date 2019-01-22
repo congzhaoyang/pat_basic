81d03c8e6ffbe8bdc5edfb8a95a78ce229b493c9
zhaoyang <congzhaoyang1@163.com>
2019-01-22 14:36:09 +0800

    1045

--- 1040.cpp
+++ 1040.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main() {
     int mod = 1000000007;
     int result = 0;
-    int countp = 0, countt = 0;
     string str;
     cin >> str;
-    int length = str.length();
-    for(int i = 0; i < length; i++) {
-        if(str[i] == 'T') {
-            countt++;
+    int len = str.length();
+    int leftNumP[len] = {0};
+    for(int i = 0; i < len; i++) { // 从左往右查P
+        if(i > 0) {
+            leftNumP[i] = leftNumP[i - 1];
+        }
+        if(str[i] == 'P') {
+            leftNumP[i]++;
         }
     }
-    for(int i = 0; i < length; i++) {
-        if(str[i] == 'P') {
-            countp++;
-        }
+    int rightNumT = 0;
+    for(int i = len - 1; i >= 0; i--) { // 从右往左查T
         if(str[i] == 'T') {
-            countt--;
-        }
-        if(str[i] == 'A') {
-            result += countp * countt;
+            rightNumT++;
+        } else if(str[i] == 'A') {
+            result = (result + leftNumP[i] * rightNumT) % mod;
         }
     }
     cout << result % mod << endl;
